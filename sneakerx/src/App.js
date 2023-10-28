beabb81d4961829b9f7104e057882a3f6e4862ec
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-10-28 11:32:04 +0200

    chore: checked

--- sneakerx/src/App.js
+++ sneakerx/src/App.js
@@ -16,8 +16,8 @@ export default function App() {
 }
 
 const sneakers = [
-  {name:'Nike Runx Stretch BN',price:960.95, gender:true,rdate:2020,brand:'nike',link:'nike-men.png'},
-  {name:'Nike Airmax Bloodsport',price:1230.95, gender:true,rdate:2021,brand:'nike',link:'nike-men-1.png'},
+  {name:'Nike Runx Stretch BN',price:960.95, gender:true,rdate:2020,brand:'nike',link:'nike-men.png' },
+  {name:'Nike Airmax Bloodsport',price:1230.95, gender:true,rdate:2021,brand:'nike',link:'nike-men-1.png'} ,
   {name:'Nike AeroBAX BN',price:775.95, gender:false,rdate:2022,brand:'nike',link:'nike-women.png'},
   {name:'Nike HI-CITI Retro',price:1450.95, gender:false,rdate:2023,brand:'nike',link:'nike-women-1.png'}
 ];
